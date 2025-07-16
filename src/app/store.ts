f2622cd11b510de0edc9076b7613d9a9c58bfe45
Võ Hoàng Phúc (work) <phuc111239@gmail.com>
2025-07-16 15:54:39 +0700

    create header, pageTracker

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { mockAuthApi } from "../features/auth/services/mockAuthApi.ts";
 import authReducer from "../features/auth/store/authSlice";
+import pageReducer from "../store/pageSlice.ts";
 
 export const store = configureStore({
   reducer: {
     // [authApi.reducerPath]: authApi.reducer,
     auth: authReducer,
+    page: pageReducer,
     [mockAuthApi.reducerPath]: mockAuthApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
