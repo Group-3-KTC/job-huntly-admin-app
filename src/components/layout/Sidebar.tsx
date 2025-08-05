170ea2abd114a2be72124046c705d786ec2da91c
Võ Hoàng Phúc (work) <phuc111239@gmail.com>
2025-08-05 15:26:54 +0700

    update title, sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,24 +1,38 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import {
   ChartBar,
   List,
   Buildings,
   UsersFour,
   ReadCvLogo,
-  SidebarSimple,
+  SignOut,
+  SidebarSimple, 
 } from "@phosphor-icons/react";
 import { assets } from "../../assets/assets";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleSidebar } from "../../store/uiSlice";
+import { toggleSidebarCollapse, toggleSidebarOpen } from "../../store/uiSlice";
 import type { RootState } from "../../app/store";
 import { t } from "ttag";
 import { useCurrentLanguage } from "../../hooks/useCurrentLanguage";
+import { logout } from "../../features/auth/store/authSlice.ts";
+import { useState } from "react";
 
 const Sidebar = () => {
   const dispatch = useDispatch();
   const isCollapsed = useSelector(
-    (state: RootState) => state.ui.isSidebarCollapsed
+    (state: RootState) => state.ui.isSidebarCollapsed,
   );
+  const isSidebarOpen = useSelector(
+    (state: RootState) => state.ui.isSidebarOpen,
+  );
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+      setIsLoggingOut(true);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      dispatch(logout());
+      navigate("/login");
+  };
 
   useCurrentLanguage();
 
@@ -53,13 +67,15 @@ const Sidebar = () => {
   return (
     <div
       className={`
-        fixed top-0 left-0 min-h-full bg-white border-r p-4 py-8 z-40 border-gray-300 
+        fixed top-0 left-0 min-h-full bg-white border-r p-4 py-8 z-40 border-gray-300
         flex flex-col gap-4 shadow-lg transition-all duration-300
+        ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } md:translate-x-0
       `}
       style={{ width: isCollapsed ? "5.5rem" : "16rem" }}
     >
-      {/* Logo & toggle button */}
-      <div className="flex flex-col items-center gap-3 border-b-2 pb-4 mb-4 border-gray-200">
+      <div className="flex flex-col items-center gap-3 pb-4 mb-4 border-b-2 border-gray-200">
         <div
           className={`flex items-end gap-2 ${
             isCollapsed
@@ -84,29 +100,14 @@ const Sidebar = () => {
             Admin
           </h2>
         </div>
-
-        {/* Nút collapse */}
-        <div
-          className={`w-full flex ${
-            isCollapsed ? "justify-center" : "justify-end"
-          }`}
-        >
-          <button
-            className="text-gray-500 hover:text-blue-600"
-            onClick={() => dispatch(toggleSidebar())}
-            aria-label="Toggle Sidebar"
-          >
-            <SidebarSimple size={24} />
-          </button>
-        </div>
       </div>
 
-      {/* Menu navigation */}
-      <nav className="flex flex-col gap-[12px] font-medium text-md">
+      <nav className="flex flex-col gap-[12px] font-medium text-md flex-grow">
         {navItems.map(({ to, label, icon }) => (
           <NavLink
             key={to}
             to={to}
+            onClick={() => dispatch(toggleSidebarOpen(false))}
             className={({ isActive }) =>
               `flex items-center gap-3 px-4 py-[12px] rounded-lg transition
               ${
@@ -121,6 +122,28 @@ const Sidebar = () => {
           </NavLink>
         ))}
       </nav>
+
+      <div className="pt-4 border-t border-gray-200">
+        <button
+          className="flex items-center gap-3 px-4 py-2 w-full text-gray-500 hover:bg-blue-100 hover:text-blue-600 rounded-lg transition-colors"
+          onClick={() => dispatch(toggleSidebarCollapse())}
+          aria-label="Toggle Sidebar Collapse"
+        >
+          <SidebarSimple size={20} />
+          {!isCollapsed && <span>Collapse</span>}
+        </button>
+      </div>
+      {/* Nút Logout */}
+      <div className="">
+        <button
+          className="flex items-center gap-3 px-4 py-2 w-full text-gray-500 hover:bg-red-100 hover:text-red-600 rounded-lg transition-colors"
+          onClick={() => handleLogout()}
+          aria-label="Logout"
+        >
+          <SignOut size={20} />
+          {!isCollapsed && <span>Logout</span>}
+        </button>
+      </div>
     </div>
   );
 };
