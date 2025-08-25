849bc690efa3b914644cf3c0ff897ab7edc839dc
Võ Hoàng Phúc (work) <phuc111239@gmail.com>
2025-08-25 21:23:49 +0700

    testing runtime env

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL: string =
-  (window as any).__ENV__?.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE_URL;
+export const API_URL =
+  window.__ENV__?.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE_URL;
